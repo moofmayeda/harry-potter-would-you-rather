c9a2ae2ebe1e35b1d0ae63c9e1de1b71f5848919
moof mayeda <moofmayeda@yahoo.com>
2015-06-17 16:50:29 -0700

    Create seed file

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,6 @@
 class Pair < ActiveRecord::Base
   belongs_to :left, :class_name => 'Choice'
   belongs_to :right, :class_name => 'Choice'
+  accepts_nested_attributes_for :left
+  accepts_nested_attributes_for :right
 end
