bb6a47c1142716988e4ff7df852833432f99e073
moof mayeda <moofmayeda@yahoo.com>
2015-06-23 15:38:46 -0700

    Make columns sortable WIP

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,7 +1,17 @@
 class PairsController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def stats
-    @pairs = Pair.search(params[:search])
+    @pairs = Pair.search(params[:search]).includes(:left).order(sort_column + ' ' + sort_direction)
     @dominating_pairs = Pair.dominant(70)
     @close_pairs = Pair.close(15)
+  end
+
+private
+  def sort_column
+    column_to_db(params[:sort])
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def sortable(column)
+    if column_to_db(column) == sort_column
+      css_class = (sort_direction == "asc") ? "glyphicon glyphicon-circle-arrow-up" : "glyphicon glyphicon-circle-arrow-down"
+    else
+      css_class = nil
+    end
+    direction = (column_to_db(column) == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to "", stats_path(:sort => column, :direction => direction), class: css_class
+  end
 end
