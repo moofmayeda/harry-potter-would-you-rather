c18315e15a0eb3c2388ea12f9cba65c266dda44c
moof mayeda <moofmayeda@yahoo.com>
2015-06-25 10:01:05 -0700

    Display a random image after voting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,14 @@ class WelcomeController < ApplicationController
   def index
     offset = rand(Pair.count)
     @pair = Pair.offset(offset).first
+    @random_image = random_image
+  end
+
+private
+	def random_image
+    files = Dir.entries(
+      "#{Rails.root}/public/images" 
+    )
+    files[rand(files.length)]
   end
 end
